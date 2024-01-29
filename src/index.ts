1b20eefdb0af0d8406b290e537f7412859122958
fauzaanu <fauzaangasim@gmail.com>
2024-01-30 00:31:13 +0500

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,74 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
-
-// Export a default object containing event handlers
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
 		const url = new URL(request.url);
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+		// add more if I missed some...
+		const code_extension = [
+			'.js',
+			'.ts',
+			'.py',
+			'.java',
+			'.c',
+			'.cpp',
+			'.php',
+			'.go',
+			'.rb',
+			'.sh',
+			'.pl',
+			'.cs',
+			'.swift',
+			'.kt',
+			'.scala',
+			'.rs',
+			'.vb',
+			'.r',
+			'.d',
+			'.dart',
+			'.groovy',
+			'.lua',
+			'.pascal',
+			'.haskell',
+			'.erlang',
+			'.perl',
+			'.elixir',
+			'.clojure',
+			'.rust',
+			'.julia',
+			'.lisp',
+			'.fortran',
+			'.prolog',
+			'.ada',
+			'.scheme',
+			'.awk',
+			'.tcl',
+			'.verilog',
+			'.vhdl',
+			'.ocaml',
+			'.f#',
+			'.abap',
+			'.bash',
+			'.cobol',
+		]
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
+		if (!code_extension.some((ext) => url.pathname.endsWith(ext))) {
+			const default_redirect = 'github.com/fauzaanu/github'
+			return Response.redirect(`https://${default_redirect}`);
 		}
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
+		let githubUrl = url.toString().replace('.fauzaanu', '');
+		githubUrl = githubUrl.replace('github.com', 'raw.githubusercontent.com');
+
+		// github includes a /blob/ in the url while browsing code - but raw site does not
+		if (githubUrl.includes('/blob/')) {
+			githubUrl = githubUrl.replace('/blob/', '/');
 		}
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
+		const response = await fetch(githubUrl);
+		const code = await response.text();
+
+		const query = encodeURIComponent(`Please provide a comprehensive and enhanced explanation of the code. Include details about the purpose, functionality, and any specific techniques or concepts utilized in the code.: \n\n\`\`\`${code}\`\`\``);
+		const redirectUrl = `https://www.phind.com/search?q=${query}`;
+
+		return Response.redirect(redirectUrl);
 	},
 };
